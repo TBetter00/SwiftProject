bbc82f386d1ff2651652296292bc26faedd248d8
admin <timetime2310@gmail.com>
2024-06-22 20:11:19 -0700

    dadad

--- mentally/mentally/ContentView.swift
+++ mentally/mentally/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var Post : String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            HStack{
+                VStack{
+                    Image("Logo").resizable().frame(width: 10,height: 10)
+                    Text("Mentally").font(.title)
+                }.padding()
+                Spacer()
+                Image("ThreeLines").resizable().frame(width: 50 , height: 50).padding()
+            }
+            TextField("โพสต์ระบาย" , text: $Post).textFieldStyle(.roundedBorder).padding()
+            HStack{
+                VStack{
+                    //Image("Location").resizable()
+                    Text("หน่วยบริหารสุขภาพจิต")
+                }
+                VStack{
+                    //Image("Location").resizable()
+                    Text("ใบรับรองสุขภาพ")
+                }
+                VStack{
+                    //Image("Location").resizable()
+                    Text("จองคิวจิตแพทย์")
+                }
+            }
+            ZStack{
+                RoundedRectangle(cornerRadius: 25.0).frame(maxHeight: 100)
+                VStack{
+                    HStack{
+                        VStack{
+                            Text("แบบทดสอบสุขภาพจิตใจ").foregroundStyle(.white)
+                            Text("ตรวจสุขภาพจิตกับผู้เชี่ยวชาญ").foregroundStyle(.white)
+                        }
+                        Image("Logo").resizable().frame(width: 10, height: 10)
+                    }
+                    Button(action: {}, label: {Text("เริ่มทำแบบทดสอบ").fontWeight(.bold).foregroundStyle(.white).background(.green).clipShape(Capsule())
+                    })
+                }
+            }.padding()
         }
-        .padding()
     }
 }
 
