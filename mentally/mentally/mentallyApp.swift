6ff5f27c769bf44b2cfd1ed9a37ca6839f609294
admin <timetime2310@gmail.com>
2024-06-22 19:11:42 -0700

    mentally

---
+++ mentally/mentally/mentallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  mentallyApp.swift
+//  mentally
+//
+//  Created by admin on 22/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct mentallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
